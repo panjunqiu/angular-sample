4d6f1133262f0636bd85a9059f1f21a1718d79c1
panjq <panjq@panjqdeMac-mini.local>
2017-09-05 22:15:12 +0800

    add sample list services

---
+++ sampleList.js
@@ -0,0 +1,15 @@
+/*
+示例代码列表数据配置，后面看情况改成二级
+*/
+angular.module('sampleApp').value('sampleList', {
+	title: "Angular Sample",
+	version: "v 1.0.0",
+	list: [
+		"Readme",
+		"ModuleA",
+		"ModuleB",
+		"ModuleC",
+		"ModuleD",
+		"ModuleE"
+	]
+})
